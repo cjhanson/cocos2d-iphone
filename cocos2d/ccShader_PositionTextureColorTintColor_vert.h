6c2a7f87ec83a397a6ae220c9dd40c4b66bbc6af
CJ Hanson <cjhanson@hansoninteractive.com>
2012-07-12 04:48:08 +0700

    Updated tint shaders to match cocos2d changes

--- cocos2d/ccShader_PositionTextureColorTintColor_vert.h
+++ cocos2d/ccShader_PositionTextureColorTintColor_vert.h
@@ -4,8 +4,6 @@ attribute vec2 a_texCoord;					\n\
 attribute vec4 a_color;						\n\
 attribute vec4 a_tintColor;					\n\
 \n\
-uniform	mat4 u_MVPMatrix;					\n\
-\n\
 #ifdef GL_ES								\n\
 varying lowp vec4 v_fragmentTint;			\n\
 varying lowp vec4 v_fragmentColor;			\n\
@@ -18,7 +16,7 @@ varying vec2 v_texCoord;					\n\
 \n\
 void main()									\n\
 {											\n\
-gl_Position = u_MVPMatrix * a_position;		\n\
+gl_Position = CC_MVPMatrix * a_position;	\n\
 v_fragmentTint = a_tintColor;				\n\
 v_fragmentColor = a_color;					\n\
 v_texCoord = a_texCoord;					\n\
