28a61c4476b26110f91f5a0d41b854d2c6536152
CJ Hanson <cjhanson@hansoninteractive.com>
2012-08-20 10:12:06 +0700

    Changed tint fragment shader to work with straight alpha (non-premultiplied)

--- cocos2d/ccShader_PositionTextureColorTintColor_frag.h
+++ cocos2d/ccShader_PositionTextureColorTintColor_frag.h
@@ -11,17 +11,11 @@ uniform sampler2D CC_Texture0;				\n\
 \n\
 void main()									\n\
 {											\n\
-//Assumes incoming texture color was premultiplied with alpha											\n\
-\n\
-//Unmultiply alpha																						\n\
-vec3 t_color3	= (texture2D(CC_Texture0, v_texCoord).rgb / texture2D(CC_Texture0, v_texCoord).a);			\n\
+//Assumes incoming texture color DOES NOT use premultiplied alpha											\n\
 \n\
 //mix the texture color with our tint color (shades as opaque paint)									\n\
-vec3 color		= (t_color3 * (1.0 - v_fragmentTint.a)) + (v_fragmentTint.rgb * v_fragmentTint.a);		\n\
+vec4 color = vec4(mix(texture2D(CC_Texture0, v_texCoord).rgb, v_fragmentTint.rgb, v_fragmentTint.a), texture2D(CC_Texture0, v_texCoord).a);		\n\
 \n\
-float alpha	= texture2D(CC_Texture0, v_texCoord).a * v_fragmentColor.a;									\n\
-\n\
-//multiply alpha																						\n\
-gl_FragColor = vec4((color * v_fragmentColor.rgb)*alpha, alpha);										\n\
+gl_FragColor = color * v_fragmentColor;										\n\
 }																										\n\
 ";
